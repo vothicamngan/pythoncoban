5ba71089e8375e3975a6e42cfe241610604919db
vothicamngan <vothicamngan1995@gmail.com>
2023-09-11 21:15:47 +0700

    Buoi4

---
+++ Buoi4/thuchanh.py
@@ -0,0 +1,8 @@
+# Bai1
+print("Nhập vào họ tên: ")
+ho_ten = input()
+print(" Xin chào: ", str(ho_ten))
+print("Nhập vào năm sinh: ")
+namsinh = int(input())
+tuoi = 2023 - namsinh
+print("Bạn ", str(tuoi), " tuổi")
