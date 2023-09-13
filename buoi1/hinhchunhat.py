5c1c8ad40c9f52e08bc01fdfff91cbfd1906abef
vothicamngan <vothicamngan1995@gmail.com>
2023-09-13 14:28:06 +0700

    Hinh tròn chữ nhật

---
+++ buoi1/hinhchunhat.py
@@ -0,0 +1,21 @@
+import turtle 
+t = turtle.Turtle()
+ 
+m = 100;
+n = 150;
+ 
+# vẽ cạnh đầu tiên
+t.forward(m) # đi thẳng với m đơn vị
+t.right(90)   # quay một góc 90 độ
+ 
+# vẽ cạnh thứ hai
+t.forward(n) # đi thẳng với n đơn vị
+t.right(90)   # quay một góc 90 độ
+ 
+# vẽ cạnh thứ ba
+t.forward(m) # đi thẳng với m đơn vị
+t.right(90)   # quay một góc 90 độ
+ 
+# vẽ cạnh thứ tư
+t.forward(n) # đi thẳng với n đơn vị
+t.right(90)   # quay một góc 90 độ
