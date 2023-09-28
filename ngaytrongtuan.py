33dca1439b31d9ff4bdbd25bd38be6a5b97feab3
vothicamngan <vothicamngan1995@gmail.com>
2023-09-28 10:04:41 +0700

    Bài tập vẽ hình tròn
    Signed-off-by: vothicamngan <vothicamngan1995@gmail.com>

---
+++ ngaytrongtuan.py
@@ -0,0 +1,17 @@
+ngay_trong_tu = int(input("Nhập vào số ngày trong tuần: "))
+if ngay_trong_tu == 1:
+    print("Monday")
+elif ngay_trong_tu == 2:
+    print("Tuesday")
+elif ngay_trong_tu == 3:
+    print("Wednesday")
+elif ngay_trong_tu == 4:
+    print("Thursday")
+elif ngay_trong_tu == 5:
+    print("Friday")
+elif ngay_trong_tu == 6:
+    print("Saturday")
+elif ngay_trong_tu == 7:
+    print("Sunday")
+else:
+    print("error, out of range")
